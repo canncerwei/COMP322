d45113f076351d6d7d400dc5d49c2955c0c1b281
Andrew Wei <futian.wei@mail.mcgill.ca>
2023-01-29 20:45:12 -0500

    word_diff() done

--- assignment1_260988349 .cpp
+++ assignment1_260988349 .cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 
 bool word_diff(std::string word1, std::string word2);
+bool classical_file_diff(std::string file1, std::string file2);
+bool enhanced_file_diff(std::string file1, std::string file2);
+void list_mismatched_lines(std::string file1, std::string file2);
+void list_mismatched_words(std::string file1, std::string file2);
 
+bool word_diff(std::string word1, std::string word2){
+    return (word1.compare(word2)==0);
+}
 
+bool classical_file_diff(std::string file1, std::string file2){
+    
+    return false;
+}
 int main(int argc, char const *argv[])
 {
     /* code */
-    return 0;
+
+    std::string str1 = "Hello World";
+    std::string str2 = "hEllO World";
+    std::string str3 = "World";
+    std::string str4 = "Hello World";
+    bool result = word_diff(str1, str4); // true
+
+    std::cout << std::boolalpha << result << std::endl;
+    
 
 }
